0f70795b769f0033408598eeaf70a19b95d325f6
mishdgr8 <Mishaelmordi@gmail.com>
2024-08-29 17:50:43 +0100

    first commit but not first commit with a bunch of changes

---
+++ app/(root)/page.tsx
@@ -0,0 +1,37 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+import React from "react";
+
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.name || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250}
+          />
+        </header>
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1250 }, { currentBalance: 2500 }]}
+      />
+    </section>
+  );
+};
+
+export default Home;
